e88e7ec840f7739bfc702ccc051f7260632e9639
HussainAlmahdi <almahdi19@gmail.com>
2021-01-07 18:33:56 +0300

    socket

---
+++ app/src/main/java/com/hussainalmahdi/android/matloob/SocketInstance.kt
@@ -0,0 +1,27 @@
+package com.hussainalmahdi.android.matloob
+
+import android.app.Application
+import com.github.nkzawa.socketio.client.IO
+import com.github.nkzawa.socketio.client.Socket
+import java.net.URISyntaxException
+private const val URL = "https://yourSocketURL.com"
+
+class SocketInstance : Application() {
+    //socket.io connection url
+    private var mSocket: Socket? = null
+
+    override fun onCreate() {
+        super.onCreate()
+        try {
+//creating socket instance
+            mSocket = IO.socket(URL)
+        } catch (e: URISyntaxException) {
+            throw RuntimeException(e)
+        }
+    }
+
+    //return socket instance
+    fun getMSocket(): Socket? {
+        return mSocket
+    }
+}
